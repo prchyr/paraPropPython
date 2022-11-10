875b02a9a8574f8e39407c32d8d645a88fef3506
Alexander Kyriacou <kyriacou@uni-wuppertal.de>
2022-11-10 10:10:43 +0100

    Adding comments to permittivity and geometry files

--- geometry.py
+++ geometry.py
@@ -4,24 +4,42 @@ from permittivity import eps2m
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
-#TODO Create a general geometry class -> create closed/polygon geometry from a list of coordinates
 #TODO Create a irregular surface class -> create surface geometry from a list of coordiantes
 
+'''
+Geometry Module:
+This includes functions for creating 2D refracitve index profiles
+'''
+
 def det(v1, v2):
+    '''
+    Calculates the cross product between two 2D-vectors
+    #TODO: Consider renmaing this -> name is misleading
+    '''
     V1 = [v1.dx, v1.dz]
     V2 = [v2.dx, v2.dz]
     return np.cross(V1, V2)
 
 def area(p1, p2, p3):
+    '''
+    Calculates the area of a triangle with 3 points all contained in a 2D surface
+    p1, p2, p3 : Points with 2 coordinates (x,z)
+    '''
     A = abs((p1.x * (p2.z - p3.z) + p2.x * (p3.z - p1.z) + p3.x * (p1.z - p2.z)) / 2.0)
     return A
 
 class point:
+    '''
+    Defines a point on a 2D surface
+    '''
     def __init__(self, x, z):
         self.x = x
         self.z = z
 
 class vector:
+    '''
+    Defines a 2D vector from 2 points
+    '''
     def __init__(self, p0, p1):
         self.x0 = p0.x
         self.z0 = p0.z
@@ -33,6 +51,15 @@ class vector:
         self.arg = np.arctan2(self.dz, self.dx)
 
 class circle:
+    '''
+    Defines a circle -> used to define a circular object with a constant permittivity
+    i.e. can represent the cross section of a pipe or a sphere in a 2D permittivity profile
+
+    Inputs:
+    x, z : defines centre of circle
+    r : radius of circle
+    eps_r : permittivity of circular object
+    '''
     def __init__(self, x, z, r, eps_r = 1+0j):
         self.x = x  # X_position central
         self.z = z  # Z_position central
@@ -43,6 +70,9 @@ class circle:
         self.V = (4. / 3.) * pi * self.r ** 3
 
     def isInside(self, x1, z1):
+        '''
+        Checks in a point defined by x1 and z1 is inside of the circle
+        '''
         dx = x1 - self.x
         dz = z1 - self.z
         dr = np.sqrt(dx ** 2 + dz ** 2)
@@ -52,6 +82,14 @@ class circle:
             return False
 
 class triangle:
+    '''
+        Used to define a triangular object with a constant permittivity
+        i.e. can represent a canyon, crevass or water-filled crack
+
+        Inputs:
+        p1,p2,p3 -> points defining the triangle
+        eps_r : permittivity of circular object
+        '''
     def __init__(self, p1, p2, p3, eps_r = 1+0j):
         self.p1 = p1
         self.p2 = p2
@@ -62,6 +100,9 @@ class triangle:
         self.n = eps2m(self.eps_r)
 
     def isInside(self, x, z):
+        '''
+        Checks in a point defined by x and z are inside of the circle
+        '''
         p = point(x,z)
         p1 = self.p1
         p2 = self.p2
@@ -88,6 +129,15 @@ class triangle:
             return False
 
 class polygon:
+    '''
+    Defines an arbitary 2D shape with constant permittivity
+    -> can represent the cross-section of a volume of constant internal permittivity
+
+    inputs:
+    list_points : a python list containing points defining the premitter of the object (should be a closed path)
+    eps_r : internal refractive index
+
+    '''
     def __init__(self, list_points, eps_r=1+0j):
         self.npoints = len(list_points)
         self.eps_r = eps_r
--- permittivity.py
+++ permittivity.py
@@ -4,13 +4,34 @@ import numpy as np
 import h5py
 import util
 
+'''
+This file contains analytical functions to generate analytical permittivity/refractive-index profiles + fundamental constants
+Also includes utility functions (convert between epsilon_r and n, or calculates conductivity
+'''
+#==================================================================
 #Fundamental Constants
+#==================================================================
+
 epsilon0 = 8.85e-12 #Permittivity of Free Space
 mu0 = 4*pi*1e-7 #Permeability of Free Space
 c0 = 1./sqrt(epsilon0*mu0) #Speed of Light in Vacuum
 
-#Functions
-def eps2m(eps_c): #Return complex refractive index (m) from complex relative permittivity (eps_c)
+
+
+#==================================================================
+#Utility Functions
+#==================================================================
+
+def eps2m(eps_c):
+    '''
+    #Return complex refractive index (m) from complex relative permittivity (eps_c)
+    Input: Complex permittivity
+    -eps_c = Re(eps_r) + 1j * Im(eps_r)
+    Output: Complex refractive index
+    -m = n + 1j k
+    (n is the real part of the refractive index -> defines wave speed)
+    (k is the extinction coefficient -> defines energy loss to medium)
+    '''
     eps_r = eps_c.real
     eps_i = eps_c.imag
     n_sq = (1/2.) * (sqrt(eps_r**2 + eps_i**2) + eps_r )
@@ -21,44 +42,54 @@ def eps2m(eps_c): #Return complex refractive index (m) from complex relative per
     return m
 
 def m2eps(m):
+    '''
+    #converts from complex refactive index m = n + i*k to complex permittivity eps_r
+    Input:
+    m = n + 1j*k (n -> is the real refractive index, k is the extinction/attenuation coefficient)[unitless]
+    Output:
+    eps_c = Re(eps_r( + 1j * Im(eps_r) [unitless]
+    '''
     eps_c = m**2
     return eps_c
 
 def cond2eps_im(sigma, f):
+    '''
+    converts from conductvitiy to imaginary permittivity
+    Input:
+    f : frequency [Hz]
+    w = 2*pi*f : anggular frequnecy [1/s]
+    sigma : electrical conductvity [S = 1/Ohm]
+    Output:
+    Im(eps_r) : Imaginary permittivity [unitless]
+    '''
     w = 2*pi*f
     return sigma/(w*epsilon0)
 
-def sigma_from_p(p, sigma): #p: porosity, sigma: conductivity for p = 0
+def sigma_from_p(p, sigma):
+    '''
+    Calculates electrical conductivity of bulk ice from conducivity of ice grain/snow particles
+    p : porosity (fraction of volume with 'vacuum')
+    v : volume filling factor (fraction of volume with matter)
+    sigma : electrical conductvity [S = 1/Ohm]
+    '''
     v = 1 - p #volume filling factor
     return sigma*v*(0.68 + 0.32*v)**2
 
 def alpha(eps_c, f):
+    '''
+    Attenuation Coefficient -> Calculated from Extinction coefficient K -> used to define energy loss per unit metre and for frquency
+    Inputs:
+    -eps_c = Re(eps_r) + 1j * Im(eps_r)
+    -f : Freuqnecy [Hz]
+
+    Outputs:
+    alpha : dielectric attenuation coefficent [1/m]
+    (dielectric energy loss L = exp(-alpha*x)
+    '''
     m_c = eps2m(eps_c)
     k = m_c.imag
     w = 2*pi*f
     return 4*w*k/c0
-
-
-def pureice(x, z):
-    n_vacuum = 1.
-    n_ice = 1.78 + 1e-5j
-    n_material = n_vacuum
-    if z >= 0:
-        n_material = n_ice
-    else:
-        n_material = n_vacuum
-    return n_material
-
-def pureice_1d(z, k = 0):
-    n_ice = 1.78 + 1j*k
-    N = len(z)
-    return n_ice * np.ones(N)
-
-def southpole(z):
-    A=1.78
-    B=-0.43
-    C=-0.0132
-    return A+B*np.exp(C*z)
 
 def rho2n(rho):
     eps_r = (1 + 0.835*rho)**2
@@ -68,44 +99,9 @@ def poro2n(poro,eps_r0):
     eps_r = eps_r0*poro*(0.68 + 0.32*poro)**2
     return eps2m(eps_r)
 
-def glacier(z, data_file):
-    glacier_data = h5py.File("share/" + data_file + ".h5", 'r')
-    eps_data = glacier_data.get("eps-data")
-    z_arr = eps_data[:,0]
-    eps_r = eps_data[:,1]
-    nprof = eps2m(eps_r)
-    zmax = glacier_data.attrs["max-depth"]
-
-    n_medium = 1.0
-    if z < 0:
-        n_medium = 1.0
-    elif z >= 0 and z < zmax:
-        ii = util.findNearest(z_arr, z)
-        n_medium = nprof[ii]
-    elif z >= zmax:
-        n_medium = 1.78
-    return n_medium
-
-def glacier_layer(z, data_file, z_layer, eps_layer):
-    n_medium = glacier(z, data_file)
-    if z > z_layer:
-        n_medium = eps2m(eps_layer)
-    return n_medium
-
-
-def pure_ice_glacier(x, z, z_layer, eps_layer):
-    n_vacuum = 1.
-    n_ice = 1.78 + 1e-5j
-    n_material = n_vacuum
-    if z >= 0:
-        n_material = n_ice
-    elif z > z_layer:
-        n_medium = eps2m(eps_layer)
-    elif z < 0:
-        n_material = n_vacuum
-    return n_material
-
-freq_test = 1.35e9 #100 MHz
+#=================================================================
+#Enceladus Utilitis
+#=================================================================
 
 sigma_solid_pure_ice = 8.7e-6 #uS/m
 P_surface = 0.9
@@ -137,6 +133,117 @@ m_ice = eps2m(eps_ice)
 eps_meteor = 8.2 + 0.1558j #Taken from Herique et al. (2018) Direct Observations of Asteroid Interior and Regolith Structure: Science Measurement Requirements
 eps_vacuum = 1.0
 eps_water = 82 + 90.884j #Based on the Conductivity of Salt Water -> 5 S/m
+
+#==================================================================
+# Analytical Profiles
+#==================================================================
+
+
+def pureice(x, z, k = 1e-5):
+    '''
+    Function returns constant refractive index of ice through 2D geometry
+    m = 1.78 + 1e-5*j
+
+    Inputs:
+    -x
+    -z
+    Outputs:
+    m = n + 1j*k
+    TODO: This function is probably redundant, -> consider removing
+    '''
+    n_vacuum = 1.
+    n_ice = 1.78 + k
+    n_material = n_vacuum
+    if z >= 0:
+        n_material = n_ice
+    else:
+        n_material = n_vacuum
+    return n_material
+
+def pureice_1d(z, k = 0):
+    '''
+        Function returns constant refractive index of ice through out depth
+        m = 1.78 + 1e-5*j
+
+        Inputs:
+        -z
+        Outputs:
+        n + 1
+    '''
+    n_ice = 1.78 + 1j*k
+    N = len(z)
+    return n_ice * np.ones(N)
+
+def southpole(z):
+    '''
+    Function returns analytical south pole function
+
+    Input:
+    z : depth
+
+    output:
+    n : refractive index (real)
+    '''
+    A=1.78
+    B=-0.43
+    C=-0.0132
+    return A+B*np.exp(C*z)
+
+def exponential_profile(z, a, b, c):
+    '''
+        Function returns exponential refractive index function
+
+        Input:
+        a : asympomptic refractive index value
+        b : depth derivative
+        c : constant
+        z : depth
+
+        output:
+        n : refractive index (real)
+        '''
+    return a + b*np.exp(c*z)
+
+
+def glacier(z, data_file):
+    glacier_data = h5py.File("share/" + data_file + ".h5", 'r')
+    eps_data = glacier_data.get("eps-data")
+    z_arr = eps_data[:,0]
+    eps_r = eps_data[:,1]
+    nprof = eps2m(eps_r)
+    zmax = glacier_data.attrs["max-depth"]
+
+    n_medium = 1.0
+    if z < 0:
+        n_medium = 1.0
+    elif z >= 0 and z < zmax:
+        ii = util.findNearest(z_arr, z)
+        n_medium = nprof[ii]
+    elif z >= zmax:
+        n_medium = 1.78
+    return n_medium
+
+def glacier_layer(z, data_file, z_layer, eps_layer):
+    n_medium = glacier(z, data_file)
+    if z > z_layer:
+        n_medium = eps2m(eps_layer)
+    return n_medium
+
+def pure_ice_glacier(x, z, z_layer, eps_layer):
+    n_vacuum = 1.
+    n_ice = 1.78 + 1e-5j
+    n_material = n_vacuum
+    if z >= 0:
+        n_material = n_ice
+    elif z > z_layer:
+        n_medium = eps2m(eps_layer)
+    elif z < 0:
+        n_material = n_vacuum
+    return n_material
+
+freq_test = 1.35e9 #100 MHz
+
+
 def enceladus_2layer(z, snow_depth=100, water_depth = 500): #Create a flat layer of snow above ice
     n_snow = eps2m(eps_snow)
     n_ice = eps2m(eps_ice)
@@ -210,41 +317,3 @@ def enceladus_environ(x, z, snow_depth = 100, meteor_list = [], crevass_list = [
             n_medium = eps2m(aquifer_i.eps_r)
 
     return n_medium
-"""
-def enceladus_environ(x, z, snow_depth = 100, sinter_depth = np.nan, meteor_list = [], crevass_list = [], aquifer_list=[]): #Creates a 2 layer geometry with added meteorites (spheres), crevasses and aquifers (triangles)
-
-    if sinter_depth != np.nan:
-        n_medium = enceladus_3layer(z, snow_depth, sinter_depth)
-
-    numMeteors = len(meteor_list)
-    numCrevasses = len(crevass_list)
-    numAquifers = len(aquifer_list)
-    #Loop over meteorites
-
-    if numMeteors > 0:  
-        for i in range(numMeteors):
-            meteor_i = meteor_list[i] #must be sphere
-            if meteor_i.isInside(x,z) == True:
-                n_medium = eps2m(meteor_i.eps_r)
-                return n_medium
-            else:
-                n_medium = enceladus_2layer(z, snow_depth)
-                return n_medium
-    else:
-    for i in range(numCrevasses):
-        crevass_i = crevass_list[i]
-        if crevass_i.isInside(x,z) == True:
-            n_medium = 1.0
-            return n_medium
-        else:
-            n_medium = enceladus_2layer(z, snow_depth)
-            return n_medium
-    for i in range(numAquifers):
-        aquifer_i = aquifer_list[i]
-        if aquifer_i.isInside(x,z) == True:
-            n_medium = eps2m(aquifer_i.eps_r)
-        else:
-            n_medium = enceladus_2layer(z, snow_depth)
-
-    return n_medium
-"""
